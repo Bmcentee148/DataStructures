9d259996d58ed7211d04c9237c1512ef9c92501e
Brian McEntee <brian.m.mcentee@gmail.com>
2015-04-17 16:41:27 -0400

    skeleton of ArrayBag complete

---
+++ Bags/FixedArrayBag/ArrayBag.java
@@ -0,0 +1,63 @@
+/**
+	This uses a fixed sized array to implement the ADT bag
+	@author Brian McEntee
+	@version 1.0
+*/
+
+public class ArrayBag<T> implements BagInterface<T> {
+
+	public int getCurrentSize() {
+		//TODO
+		return -1;
+	}
+
+	public boolean isFull() {
+		//TODO
+		return false;
+	}
+
+	public boolean isEmpty() {
+		//TODO
+		return false;
+	}
+
+	public void clear() {
+		//TODO
+	}
+
+	public boolean add(T newEntry) {
+		//TODO
+		return false;
+	}
+
+	public T remove() {
+		//TODO
+		return null;
+	}
+
+	public T remove(T entry) {
+		//TODO
+		return null;
+	}
+
+	public int getFrequencyOf(T entry) {
+		//TODO
+		return 0;
+	}
+
+	public boolean contains(T entry) {
+		//TODO
+		return false;
+	}
+
+	public T[] toArray() {
+		//TODO
+		return null;
+	}
+
+
+
+
+
+
+}
