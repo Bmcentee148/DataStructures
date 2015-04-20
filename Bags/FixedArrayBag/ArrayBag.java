5039bad5a19c979ab220d71d0031e90ad4034bbc
Brian McEntee <brian.m.mcentee@gmail.com>
2015-04-19 21:56:57 -0400

    added defining comments to all methods of implementation

--- Bags/FixedArrayBag/ArrayBag.java
+++ Bags/FixedArrayBag/ArrayBag.java
@@ -3,6 +3,7 @@
 	@author Brian McEntee
 	@version 1.0
 */
+import java.util.Arrays;
 
 public class ArrayBag<T> implements BagInterface<T> {
 
@@ -22,22 +23,32 @@ public class ArrayBag<T> implements BagInterface<T> {
 		this(DEFAULT_CAPACITY); 
 	}
 
+	/** Gets the current number of entries in the bag
+		@return the integer number of entries in the bag */
 	public int getCurrentSize() {
 		return numEntries;
 	}
 
+	/** Determines whether or not the bag is full
+		@return true if the bag is full, else false */
 	public boolean isFull() {
 		return numEntries == bag.length;
 	}
 
+	/** Determines whether or not the bag is empty
+		@return true if the bag is empty, else false */
 	public boolean isEmpty() {
 		return numEntries == 0;
 	}
 
+	/** Removes all of the items from the bag */
 	public void clear() {
 		//TODO
 	}
 
+	/** Adds a new item to the bag
+		@param newEntry the item to add to the bag
+		@return true if the item was successfully added, else false */
 	public boolean add(T newEntry) {
 		boolean status = false;
 		if(newEntry!= null && !isFull()) {
@@ -48,32 +59,50 @@ public class ArrayBag<T> implements BagInterface<T> {
 		return status;
 	}
 
+	/** Removes an arbitrary item from the bag
+		@return the item removed from the bag if successfull, or null */
 	public T remove() {
 		//TODO
 		return null;
 	}
 
+
+	/** Removes one occurrence of the given item from the bag
+		@param entry the item to be removed from the bag
+		@return the removed entry if successful, else null */
 	public T remove(T entry) {
 		//TODO
 		return null;
 	}
 
+	/** Determines how many times the given item occurs in the bag
+		@param entry the item to be counted
+		@return the number of times entry appears in the bag */
 	public int getFrequencyOf(T entry) {
 		//TODO
 		return 0;
 	}
 
+	/** Determines whether or not the given item appears in the bag
+		@param entry the item to search for
+		@return true if the bag contains the item, else false */
 	public boolean contains(T entry) {
 		//TODO
 		return false;
 	}
 
+	
+	/** Creates an array containing all of the elements in the bag
+		@return the newly allocated array containing all of the bags elements */
 	public T[] toArray() {
+	/*
 		@SuppressWarnings("unchecked")
 		T[] result = (T[])new Object[numEntries];
 		for(int i = 0; i < numEntries; i++)
 			result[i] = (T)bag[i];
 		return result;
+	*/
+		return Arrays.copyOf(bag,numEntries); //quicker
 	}
 
 
